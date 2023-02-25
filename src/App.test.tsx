a8adfd8762cbe1cf1967a5ad0f1be76722fd6ebf
Martin Palmieri <martinpalmieri@gmail.com>
2023-02-25 22:14:31 +0100

    Add component testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,107 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { createRoot } from 'react-dom/client';
+import { act } from 'react-dom/test-utils';
+import { screen } from '@testing-library/react';
 import App from './App';
+import Header from './components/header/header';
+import List from './components/flights-list/flights-list';
+import { FlightsContext } from './context/flights.context';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let container: any = null;
+const setFlights = () => null;
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
+
+it('renders without crashing', () => {
+  // eslint-disable-next-line testing-library/no-unnecessary-act
+  act(() => {
+    createRoot(container!).render(<App />);
+  });
+});
+
+it('FlightsApp title', () => {
+  // eslint-disable-next-line testing-library/no-unnecessary-act
+  act(() => {
+    createRoot(container!).render(<App />);
+  });
+  expect(screen.getByText('FlightsApp')).toBeInTheDocument();
+});
+
+it("App shows initial 'Start typing to search' message", () => {
+  // eslint-disable-next-line testing-library/no-unnecessary-act
+  act(() => {
+    createRoot(container!).render(
+      <FlightsContext.Provider value={{ flights: [], setFlights }}>
+        <Header />
+        <List />
+      </FlightsContext.Provider>,
+    );
+  });
+  expect(
+    screen.getByText('Start typing to search for flights...'),
+  ).toBeInTheDocument();
+});
+
+it("App doest not show initial 'Start typing to search' message' if context has flights", () => {
+  // eslint-disable-next-line testing-library/no-unnecessary-act
+  act(() => {
+    createRoot(container!).render(
+      <FlightsContext.Provider
+        value={{
+          flights: [
+            {
+              flightIdentifier: 'D20190401UA969',
+              flightNumber: 'UA 969',
+              airport: 'San Francisco',
+              expectedTime: '14:50',
+              originalTime: '14:50',
+              url: '/en/departures/flight/D20190401UA969/',
+              score: '70.55272',
+            },
+          ],
+          setFlights,
+        }}
+      >
+        <Header />
+        <List />
+      </FlightsContext.Provider>,
+    );
+  });
+  expect(
+    screen.queryByText('Start typing to search for flights...'),
+  ).not.toBeInTheDocument();
+});
+
+it('App show flight number if context has flights', () => {
+  // eslint-disable-next-line testing-library/no-unnecessary-act
+  act(() => {
+    createRoot(container!).render(
+      <FlightsContext.Provider
+        value={{
+          flights: [
+            {
+              flightIdentifier: 'D20190401UA969',
+              flightNumber: 'UA 969',
+              airport: 'San Francisco',
+              expectedTime: '14:50',
+              originalTime: '14:50',
+              url: '/en/departures/flight/D20190401UA969/',
+              score: '70.55272',
+            },
+          ],
+          setFlights,
+        }}
+      >
+        <Header />
+        <List />
+      </FlightsContext.Provider>,
+    );
+  });
+  expect(screen.getByText('UA 969')).toBeInTheDocument();
 });
